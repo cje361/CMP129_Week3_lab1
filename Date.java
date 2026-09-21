60ee0ab0a37c862b55bca7f931940cf5fad7203b
cje361 <edwards.christian494@student.ccm.edu>
2026-09-21 13:29:15 -0400

    completed lab5

--- Date.java
+++ Date.java
@@ -1,3 +1,22 @@
 public class Date {
     
+    private static final String[] months = {
+        "January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December"
+    };
+
+    private int month;
+    private int day;
+    private int year;
+
+    public Date() {
+        this(1, 1, 2000);
+    }
+
+    public Date(int month, int day, int year) {
+        setMonth(month);
+        setDay(day);
+        setYear(year);
+    }
+
+    
 }
