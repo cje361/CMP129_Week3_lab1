546e52928558aab720767c729ee92ad14f04778d
cje361 <edwards.christian494@student.ccm.edu>
2026-09-22 17:41:01 -0400

    completed lab5

--- EmployeeTest.java
+++ EmployeeTest.java
@@ -1,3 +1,14 @@
 public class EmployeeTest {
-    
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Susan Meyers", 47899, "Accounting", "Vice President");
+        Employee employee2 = new Employee("Mark Jones", 39119, "IT", "Programmer");
+        Employee employee3 = new Employee("Joy Rogers", 81774, "Manufacturing", "Engineer");
+
+        employee1.display();
+        System.out.println();
+        employee2.display();
+        System.out.println();
+        employee3.display();
+        
+    }
 }
