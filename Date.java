5a47769ed823a5a5e01020a05f44b32fd3281110
cje361 <edwards.christian494@student.ccm.edu>
2026-09-22 14:34:04 -0400

    completed lab5

--- Date.java
+++ Date.java
@@ -18,5 +18,51 @@ public class Date {
         setYear(year);
     }
 
-    
+    public void setMonth(int month) {
+        if (month < 1 || month > 12) {
+            throw new IllegalArgumentException("Month must be between 1 and 12.");
+        }
+        this.month = month;
+    }
+
+    public void setDay(int day) {
+        if (day < 1 || day > 31) {
+            throw new IllegalArgumentException("Day must be between 1 and 31.");
+        }
+        this.day = day;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void displayNumeric() {
+        System.out.println(month + "/" + day + "/" + year);
+    }
+
+    public void displayMonthFirst() {
+        System.out.println(getMonthName() + " " + day + ", " + year);
+    }
+
+    public void displayDayFirst() {
+        System.out.println(day + " " + getMonthName() + " " + year);
+    }
+
+    private String getMonthName() {
+        return months[month - 1];
+    }
+
+
 }
--- DateTest.java
+++ DateTest.java
@@ -1,3 +1,40 @@
+import java.util.Scanner;
+
 public class DateTest {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Date date = new Date();
+
+       boolean valid = false;
+       while (!valid) {
+        System.out.println("Enter the month (1-12): ");
+        int month = input.nextInt();
+        try {
+            date.setMonth(month);
+            valid = true;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid" + e.getMessage());
+        }
+       } 
+
+       valid = false;
+       while (!valid) {
+        System.out.println("Enter the day (1-31): ");
+        int day = input.nextInt();
+        try {
+            date.setDay(day);
+            valid = true;
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid" + e.getMessage());
+        }
+       }
+
+       System.out.println("Enter the year: ");
+       date.setYear(input.nextInt());
+
+       System.out.println();
+       date.displayNumeric();
+       date.displayMonthFirst();
+       date.displayDayFirst();
+    }
 }
